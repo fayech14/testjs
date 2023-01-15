612e6c15f9455faa401cc602d8261df56aed9511
hassan <hssan.fayche@gmail.com>
2023-01-15 16:27:12 +0100

    controle de saisie

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
-let name = prompt("add your name :")
-let lastname = prompt("add your last name :")
-console.log("welcome " + name + " " + lastname)
+let num1 = prompt(" donner un num1 ")
+let num2 = prompt(" donner un num2 ")
+if (num1>num2){
+    console.log(num1 + " est superieure à " + num2)
+}
+else if (num1==num2){
+    console.log(num1 + " est egale à " + num2)
+}
+else{
+    console.log(num1 + " est inferieure à " + num2)
+}
